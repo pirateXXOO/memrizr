8410e38e02a792cdf047b5b35c5e8990fe0dff30
shuai Wang <shuaiwang@bjyz-dl-001.autox.sz>
2022-05-26 15:46:09 +0800

    complete lesson-27

--- account-client/src/main.js
+++ account-client/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './routes.js';
+import './index.css'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
